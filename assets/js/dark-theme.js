403751e3e04f91db83483949d383711dae227c87
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-25 03:35:07 +0300

    Son durumda ki Tema seçiminin localStorage'a kaydedilmesi işlemi yapıldı. theme'in durumuna göre gerekli class eklemesi verildi.

--- assets/js/dark-theme.js
+++ assets/js/dark-theme.js
@@ -1,22 +1,47 @@
 const body = document.querySelector('body');
 const themeBtn = document.querySelector('.theme-selector-btn');
 
+let saveTheme = JSON.parse(localStorage.getItem('theme')) || [];
+
+function saveThemeToLocalStorage(){
+    return localStorage.setItem('theme',JSON.stringify(saveTheme));
+}
+
 themeBtn.addEventListener('click',switchTheme);
 
 function switchTheme(){
+    saveTheme = [];
     if(body.classList.contains('dark-theme')){
         body.classList.remove('dark-theme');
         themeBtn.innerHTML = 
         `
         DARK
         <img src="assets/img/moon.svg" alt="">
-        `
+        `;
+        saveTheme.push(
+            {
+                theme: 'light'
+            }
+        )
+        saveThemeToLocalStorage();
     }else{
         body.classList.add('dark-theme');
         themeBtn.innerHTML = 
         `
         LIGHT
         <img src="assets/img/light-theme-icon.svg" alt="">
-        `
+        `;
+        saveTheme.push(
+            {
+                theme: 'dark'
+            }
+        )
+        saveThemeToLocalStorage();
     }
+}
+
+if(saveTheme[0].theme == 'light'){
+    body.classList.remove('dark-theme');
+}else{
+    body.classList.add('dark-theme');
 }
